588226aab0c8b4edd9f45ecc835170d58ed5fb48
MADERU-CM <cliv.moyia@student.moringaschool.com>
2023-07-13 17:23:44 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// index.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    const main = document.querySelector('main#main');
+    main.remove();
+  
+    const newHeader = document.createElement('h1');
+    newHeader.id = 'victory';
+    newHeader.textContent = "YOUR-NAME is the champion"; 
+    document.body.appendChild(newHeader);
+  });
+  
